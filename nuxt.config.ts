518522f09cbb61fd6280a7caf4957db5e7a71894
florent giraud <hello@florent.dev>
2025-01-10 22:32:42 -0500

    chore: clear nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
-  runtimeConfig: {
-    database: {
-      url: process.env.NUXT_DB_FILE_NAME
-    }
-  },
-  nitro: {
-    experimental: {
-      tasks: true
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image']
 })
